01e12e262a28dc12a0c408ef98ea8957ede07b06
Innan Plínio Rangel Amorim <45242384+innanplinio@users.noreply.github.com>
2021-09-14 15:32:25 -0300

    att

--- Code/first_collection.py
+++ Code/first_collection.py
@@ -39,9 +39,9 @@ def getCurrentTime():
 def get_ad_account():
     
     if secret != '-':
-        FacebookAdsApi.init(access_token=token, app_secret=secret, api_version='v9.0')
+        FacebookAdsApi.init(access_token=token, app_secret=secret, api_version='v10.0')
     else:
-        FacebookAdsApi.init(access_token=token, api_version='v9.0')
+        FacebookAdsApi.init(access_token=token, api_version='v10.0')
     account = AdAccount('act_' + act_id)
     return account
 
